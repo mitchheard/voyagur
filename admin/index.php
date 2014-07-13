3a4e7d20ad3ebe86be0d55e090143e28ca5860d7
root <root@precise64.(none)>
2014-07-13 15:09:41 +0000

    Added phpinfo() on admin page for troubleshoot

--- admin/index.php
+++ admin/index.php
@@ -4,4 +4,7 @@
 	<li><a href = 'db_base.php'>Clean DB setup page (click and voyagur db will be overwritten to stock)</a></li>
 	<li><a href ='http://44.44.44.10/phpmyadmin/'>Php MyAdmin</a></li>
 
+
+	<?php phpinfo(); ?>
+
 </ul>
