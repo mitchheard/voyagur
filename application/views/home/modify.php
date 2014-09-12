593477d7cb5a22d3cc0a5388434d9c28699355b2
Kevin Marion <kevinmarion@Kevins-MacBook-Air.local>
2014-09-11 19:28:43 -0700

    Added in the getvoyagers object retrieval

--- application/views/home/modify.php
+++ application/views/home/modify.php
@@ -8,13 +8,9 @@ require_once "/var/www/html/application/backend/voyage.inc";
 //$voyage_info = Voyage::get(4);
 
 $voyage_info = new Voyage();
-$voyage_main = $voyage_info->getVoyage(1);
+$voyage_main = $voyage_info->getVoyagers(1);
 
 
 echo "<br />";
 echo "<br />";
-echo "Voyage name: " . $voyage_main->name ;
-echo "<br />";
-echo "begin: " . $voyage_main->begin_date ;
-echo "<br />";
-echo "end: " . $voyage_main->end_date ;
+print_r($voyage_main);
