f2b9e9abf9aed87bdc92e8477529691b73186b82
Kevin Marion <kevinmarion@Kevins-MacBook-Air.local>
2014-09-11 18:45:06 -0700

    added version 1 voyage class & db version 9/11/14

--- application/views/home/modify.php
+++ application/views/home/modify.php
@@ -1,1 +1,20 @@
-Welcome back!
+<?php
+
+
+require_once "/var/www/html/application/backend/voyage.inc";
+
+
+
+//$voyage_info = Voyage::get(4);
+
+$voyage_info = new Voyage();
+$voyage_main = $voyage_info->getVoyage(1);
+
+
+echo "<br />";
+echo "<br />";
+echo "Voyage name: " . $voyage_main->name ;
+echo "<br />";
+echo "begin: " . $voyage_main->begin_date ;
+echo "<br />";
+echo "end: " . $voyage_main->end_date ;
