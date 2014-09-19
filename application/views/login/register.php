a69dec9d969abb920c64425d2d5dcf0a5366767d
Mitch Heard <mitchheard@gmail.com>
2014-09-19 01:05:49 -0500

    styled the register user submission form

--- application/views/login/register.php
+++ application/views/login/register.php
@@ -4,58 +4,34 @@
     <?php $this->renderFeedbackMessages(); ?>
 
     <div class="register-default-box">
-        <h1>Register</h1>
-        <!-- register form -->
-        <form method="post" action="<?php echo URL; ?>login/register_action" name="registerform">
-            <!-- the user name input field uses a HTML5 pattern check -->
-            <label for="login_input_username">
-                Username
-                <span style="display: block; font-size: 14px; color: #999;">(only letters and numbers, 2 to 64 characters)</span>
-            </label>
-            <input id="login_input_username" class="login_input" type="text" pattern="[a-zA-Z0-9]{2,64}" name="user_name" required />
-            <!-- the email input field uses a HTML5 email type check -->
-            <label for="login_input_email">
-                User's email
-                <span style="display: block; font-size: 14px; color: #999;">
-                    (please provide a <span style="text-decoration: underline; color: mediumvioletred;">real email address</span>,
-                    you'll get a verification mail with an activation link)
-                </span>
-            </label>
-            <input id="login_input_email" class="login_input" type="email" name="user_email" required />
-            <label for="login_input_password_new">
-                Password (min. 6 characters!
-                <span class="login-form-password-pattern-reminder">
-                    Please note: using a long sentence as a password is much much safer then something like "!c00lPa$$w0rd").
-                    Have a look on
-                    <a href="http://security.stackexchange.com/questions/6095/xkcd-936-short-complex-password-or-long-dictionary-passphrase">
-                        this interesting security.stackoverflow.com thread
-                    </a>.
-                </span>
-            </label>
-            <input id="login_input_password_new" class="login_input" type="password" name="user_password_new" pattern=".{6,}" required autocomplete="off" />
-            <label for="login_input_password_repeat">Repeat password</label>
-            <input id="login_input_password_repeat" class="login_input" type="password" name="user_password_repeat" pattern=".{6,}" required autocomplete="off" />
-            <!-- show the captcha by calling the login/showCaptcha-method in the src attribute of the img tag -->
-            <!-- to avoid weird with-slash-without-slash issues: simply always use the URL constant here -->
+    <form class="form-signin" role="form" method="post" action="<?php echo URL; ?>login/register_action" name="registerform">
+        <h2 class="form-signin-heading">Create Account</h2>
+        <input id="login_input_username" name="user_name" pattern="[a-zA-Z0-9]{2,64}" type="text" class="form-control" placeholder="Username" required />
+        <span>(letters and numbers, 2-64 characters)</span>
+        <input id="login_input_email" name="user_email" type="email" class="form-control" placeholder="Email" required autofocus />
+        <span>(for a verification mail with an activation link)</span>
+        <input id="login_input_password_new" name="user_password_new" pattern=".{6,}" type="password" class="form-control" placeholder="Password" required autocomplete="off" />
+        <span>(min. 6 characters)</span>
+        <input id="login_input_password_repeat" name="user_password_repeat" pattern=".{6,}" type="password" class="form-control" placeholder="Repeat Password" required autocomplete="off" />
+        <!-- show the captcha by calling the login/showCaptcha-method in the src attribute of the img tag -->
+        <!-- to avoid weird with-slash-without-slash issues: simply always use the URL constant here -->
+        <div>
+            <p>Please enter the following characters</p>
             <img id="captcha" src="<?php echo URL; ?>login/showCaptcha" />
-            <span style="display: block; font-size: 11px; color: #999; margin-bottom: 10px">
-                <!-- quick & dirty captcha reloader -->
-                <a href="#" onclick="document.getElementById('captcha').src = '<?php echo URL; ?>login/showCaptcha?' + Math.random(); return false">[ Reload Captcha ]</a>
-            </span>
-            <label>
-                Please enter these characters
-                <span style="display: block; font-size: 11px; color: #999;">
-                    Please note: This captcha will be generated when the img tag requests the captcha-generation
-                    (and a real image) from YOURURL/login/showcaptcha. As this is a client-side triggered request, the
-                    $_SESSION["captcha"] dump in the footer will not show the captcha characters. The captcha generation
-                    happens AFTER the rendering of the footer.
-                </span>
-            </label>
-            <input type="text" name="captcha" required />
-            <input type="submit"  name="register" value="Register" />
+            <!-- quick & dirty captcha reloader -->
+            <a href="#" onclick="document.getElementById('captcha').src = '<?php echo URL; ?>login/showCaptcha?' + Math.random(); return false">[ Reload Captcha ]</a>
+            <input type="text" name="captcha" class="form-control" required />
+        </div>
+        <span style="display: block; font-size: 11px; color: #999;">
+            Please note: This captcha will be generated when the img tag requests the captcha-generation
+            (and a real image) from YOURURL/login/showcaptcha. As this is a client-side triggered request, the
+            $_SESSION["captcha"] dump in the footer will not show the captcha characters. The captcha generation
+            happens AFTER the rendering of the footer.
+        </span>
+        <!-- <input type="checkbox" value="remember-me"> <span>Remember me</span> -->
+        <button class="btn btn-lg btn-primary btn-block" name="register" type="submit">Register</button>
+    </form>
 
-        </form>
-    </div>
 
     <?php if (FACEBOOK_LOGIN == true) { ?>
         <div class="register-facebook-box">
